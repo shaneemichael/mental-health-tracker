0213d84e0aaeb01d391487dafec4ab96ee887606
shaneemichael <shanemichaeltanata@gmail.com>
2024-08-28 16:14:25 +0700

    add allowed hosts

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shane-michael-mentalhealthtracker.pbp.cs.ui.ac.id"]
